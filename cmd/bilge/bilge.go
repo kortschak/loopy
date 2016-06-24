8d9cc406bc18b28b2e2cf2851ba548638bd4df71
kortschak <dan.kortschak@adelaide.edu.au>
2016-06-24 13:09:09 +0930

    cmd/bilge: allow non-WF complexity calculation

--- cmd/bilge/bilge.go
+++ cmd/bilge/bilge.go
@@ -15,6 +15,7 @@ import (
 	"github.com/biogo/biogo/complexity"
 	"github.com/biogo/biogo/io/seqio"
 	"github.com/biogo/biogo/io/seqio/fasta"
+	"github.com/biogo/biogo/seq"
 	"github.com/biogo/biogo/seq/linear"
 )
 
@@ -22,14 +23,21 @@ var (
 	in     = flag.String("in", "", "specify input gff file (required)")
 	thresh = flag.Float64("thresh", 6, "specify minimum total sequence complexity")
 	dist   = flag.Bool("dist", false, "only calculate complexity distribution")
+	typ    = flag.Int("type", 0, "specify complexity calculation function (0 - WF, 1 - entropic, 2 - Z")
 )
 
 func main() {
 	flag.Parse()
-	if *in == "" {
+	if *in == "" || *typ < 0 || 2 < *typ {
 		flag.Usage()
 		os.Exit(1)
 	}
+
+	cfn := []func(s seq.Sequence, start, end int) (float64, error){
+		0: complexity.WF,
+		1: complexity.Entropic,
+		2: complexity.Z,
+	}[*typ]
 
 	f, err := os.Open(*in)
 	if err != nil {
@@ -42,13 +50,13 @@ func main() {
 		seq := sc.Seq().(*linear.Seq)
 
 		// err is always nil for a linear.Seq Start() and End().
-		cz, _ := complexity.WF(seq, seq.Start(), seq.End())
+		c, _ := cfn(seq, seq.Start(), seq.End())
 
 		if *dist {
-			fmt.Printf("%s\t%v\n", seq.Name(), cz)
+			fmt.Printf("%s\t%v\n", seq.Name(), c)
 			continue
 		}
-		if cz >= *thresh {
+		if c >= *thresh {
 			fmt.Printf("%60a\n", seq)
 		}
 	}
